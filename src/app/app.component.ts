df6321c01df7e9daedce17d01e901f3c619014bb
Ajinkya <hatwarajinkya@gmail.com>
2022-01-11 23:35:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup } from '@angular/forms';
+import { User } from './userModel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myform';
+
+  // contact: User;
+  // constructor(private contact: User) {}
+
+  ngOnInit() {
+    // this.contact = {
+    //   name: 'akshay',
+    //   email: 'akshay@gmail.com',
+    //   gender: 'male',
+    //   isMarried: true,
+    //   country: 'India',
+    //   address: { city: 'Nagpur', street: 'Ring road', pincode: '441100' },
+    // };
+  }
+
+  submitData(data: any) {
+    console.log(data);
+  }
 }
+
+// export interface User {
+//   name: string;
+//   email: string;
+//   gender: string;
+//   isMarried: boolean;
+//   country: string;
+//   address: {
+//     city: string;
+//     street: string;
+//     pincode: string;
+//   };
+// }
---
+++ src/app/userModel.ts
@@ -0,0 +1,12 @@
+export interface User {
+  name: string;
+  email: string;
+  gender: string;
+  isMarried: boolean;
+  country: string;
+  address: {
+    city: string;
+    street: string;
+    pincode: string;
+  };
+}
